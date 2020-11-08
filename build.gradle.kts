9ad54591b5c442828c2110bb61d1e9a2784c5c5b
Roman Kolesnikov <rk2001.mailbox@gmail.com>
2020-11-08 21:51:39 +0100

    Add ktlint and idea plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+  kotlin("jvm") version "1.4.10"
+  id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
+  idea
 }
 group = "me.roman"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+  kotlinOptions.jvmTarget = "1.8"
 }

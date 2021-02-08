38b39cd50a4a3fa6ca1f8a0a1d666e35cf7b1c9c
Roman Kolesnikov <rk2001.mailbox@gmail.com>
2021-02-08 15:47:01 +0100

    Move spread to own model

---
+++ src/main/kotlin/spready/ui/SpreadModel.kt
@@ -0,0 +1,65 @@
+package spready.ui
+
+import javafx.beans.property.SimpleObjectProperty
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import spready.spread.Spread
+import spready.ui.menu.Open
+import spready.ui.menu.Save
+import spready.ui.menu.SpreadFileEvent
+import spready.ui.sheet.Err
+import spready.ui.sheet.EvalStatusEvent
+import spready.ui.sheet.NewSpreadEvent
+import spready.ui.sheet.Ok
+import tornadofx.ViewModel
+import tornadofx.getValue
+import tornadofx.setValue
+import java.io.File
+
+class SpreadModel : ViewModel() {
+    val spreadProperty = SimpleObjectProperty(Spread())
+    var spread: Spread by spreadProperty
+
+    init {
+        subscribe<SpreadFileEvent> {
+            when (it.action) {
+                Open -> loadFromFile(it.file)
+                Save -> saveToFile(it.file)
+            }
+        }
+    }
+
+    private fun loadFromFile(file: File) {
+        val result = try {
+            spread = Json.decodeFromString(file.readText())
+
+            val maxSizes = spread.allResults.keys.fold(Pair(10, 10)) { acc, cell ->
+                var newPair = acc
+
+                if (cell.row > newPair.first) {
+                    newPair = Pair(cell.row, newPair.second)
+                }
+
+                if (cell.col > newPair.second) {
+                    newPair = Pair(newPair.first, cell.col)
+                }
+
+                newPair
+            }
+
+            fire(NewSpreadEvent(maxSizes.first, maxSizes.second))
+            Ok
+        } catch (ex: SerializationException) {
+            Err(ex.message ?: "Can't open file!")
+        }
+
+        fire(EvalStatusEvent(result))
+    }
+
+    private fun saveToFile(file: File) {
+        val text = Json.encodeToString(spread)
+        file.writeText(text)
+    }
+}
--- src/main/kotlin/spready/ui/sheet/SheetModel.kt
+++ src/main/kotlin/spready/ui/sheet/SheetModel.kt
@@ -2,111 +2,63 @@ package spready.ui.sheet
 
 import javafx.beans.property.SimpleSetProperty
 import javafx.beans.property.SimpleStringProperty
-import kotlinx.serialization.SerializationException
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import spready.lisp.EvalException
 import spready.lisp.sexpr.Cell
-import spready.spread.Spread
 import spready.spread.SpreadException
-import spready.ui.menu.Open
-import spready.ui.menu.Save
-import spready.ui.menu.SpreadFileEvent
+import spready.ui.SpreadModel
 import tornadofx.ViewModel
 import tornadofx.asObservable
 import tornadofx.getValue
 import tornadofx.setValue
-import java.io.File
 
-class SheetModel(private var spread: Spread = Spread()) : ViewModel() {
+class SheetModel : ViewModel() {
+    private val spreadModel: SpreadModel by inject()
+
+    private val spread by spreadModel.spreadProperty
 
     private var currentCell: Cell? = null
 
     val currentInputProperty = SimpleStringProperty("")
     private var currentInput by currentInputProperty
 
-    val changedCellsProperty = SimpleSetProperty<Cell>()
+    val changedCellsProperty = SimpleSetProperty(emptySet<Cell>().asObservable())
     private var changedCells by changedCellsProperty
 
     val allResults: Map<Cell, String>
         get() = spread.allResults
 
-    init {
-        subscribe<SpreadFileEvent> {
-            when (it.action) {
-                Open -> loadFromFile(it.file)
-                Save -> saveToFile(it.file)
-            }
-        }
-    }
-
     fun evalInput() {
         val cell = currentCell
 
         if (cell != null) {
-            if (currentInput == "") {
+            val result = if (currentInput == "") {
                 try {
                     spread -= cell
+                    Ok
                 } catch (_: EvalException) {
+                    Ok
                 } catch (ex: SpreadException) {
-                    fire(EvalStatusEvent(Err(ex.message)))
+                    Err(ex.message)
                 }
             } else {
                 try {
                     spread.setCell(cell, currentInput)
-                    fire(EvalStatusEvent(Ok))
+                    changedCells = spread.changedCells.asObservable()
+                    Ok
                 } catch (ex: EvalException) {
-                    fire(EvalStatusEvent(Err(ex.message)))
+                    Err(ex.message)
                 } catch (ex: SpreadException) {
-                    fire(EvalStatusEvent(Err(ex.message)))
+                    Err(ex.message)
                 }
             }
-        }
 
-        changedCells = spread.changedCells.asObservable()
+            fire(EvalStatusEvent(result))
+        }
     }
 
     fun chooseCell(cell: Cell) {
         currentCell = cell
 
         currentInput = spread.getInput(cell) ?: ""
-    }
-
-    private fun loadFromFile(file: File) {
-        try {
-            spread = Json.decodeFromString(file.readText())
-            var maxRow = 0
-            var maxCol = 0
-
-            for (item in spread.allResults) {
-                val cell = item.key
-
-                if (cell.row > maxRow) {
-                    maxRow = cell.row
-                }
-
-                if (cell.col > maxCol) {
-                    maxCol = cell.col
-                }
-            }
-
-            if (maxRow < 10) {
-                maxRow = 10
-            }
-
-            if (maxCol < 10) {
-                maxCol = 10
-            }
-
-            fire(NewSpreadEvent(maxRow, maxCol))
-        } catch (ex: SerializationException) {
-            fire(EvalStatusEvent(Err(ex.message ?: "Can't open file!")))
-        }
-    }
-
-    private fun saveToFile(file: File) {
-        val text = Json.encodeToString(spread)
-        file.writeText(text)
     }
 }
